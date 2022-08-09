e362fe29b91451daaf7456d353c0357289823fe6
Xavier Prospero <xavierprospero@berkeley.edu>
2022-08-08 23:48:53 -0700

    lense -> lens. whoops

--- gicameramodel.py
+++ gicameramodel.py
@@ -1,7 +1,7 @@
 '''
-    A single lense model.
+    A single lens model.
     Main components are:
-    1. BiConvex lense.
+    1. BiConvex lens.
     2. A point source light.
     3. A sensor of size h * h in mm and pixel dimension M * M
 '''
@@ -29,7 +29,7 @@ class Sensor:
         # @param _x the x coordinate in mm.
         # @param _y the y coordinate in mm.
         if abs(_x) > self.h / 2 or abs(_y) > self.h / 2:
-            print("Sensor::pixelAt() given out of bounds pixel")
+            print("Sensor::pixelAt() given out of bounds pixel {} {}".format(_x, _y))
             return None
 
         pixelHeight = self.h / self.M
@@ -75,22 +75,22 @@ class Sensor:
                     self.sensor[pixelY][pixelX] += self.sensor[int(self.M // 2 + yAxisPixel)][midX]
 
 
-## A BiConvex lense that models simple ray tracing optics.
-# R1: Radius of lense on subject side.
-# R2: Radius of lense on sensor side.
-# T:  Thickness of lense at center
-# OD: The aperture of the lense.
+## A BiConvex lens that models simple ray tracing optics.
+# R1: Radius of lens on subject side.
+# R2: Radius of lens on sensor side.
+# T:  Thickness of lens at center
+# OD: The aperture of the lens.
 # We assume that index of refraction of air is 1.
-class Lense:
+class Lens:
     def __init__(self, _R1, _R2, _T, _OD):
         ## Constructer.
-        # @param _R1 Radius of lense on subject side.
-        # @param _R2 Radius of lense on sensor side.
-        # @param _T  Thickness of lense at center
-        # @param _OD The aperture of the lense.
+        # @param _R1 Radius of lens on subject side.
+        # @param _R2 Radius of lens on sensor side.
+        # @param _T  Thickness of lens at center
+        # @param _OD The aperture of the lens.
         self.OD = _OD
-        self.lense1 = Circle(-_R1 + (_T / 2), 0, _R1)
-        self.lense2 = Circle(_R2 - (_T / 2), 0, _R2)
+        self.lens1 = Circle(-_R1 + (_T / 2), 0, _R1)
+        self.lens2 = Circle(_R2 - (_T / 2), 0, _R2)
         self.n_air = 1.0
         self.n_glass = 1.5168
         #TODO allow for refraction index based off of wavelength.
@@ -98,51 +98,51 @@ class Lense:
     def refract(self, _ray):
         ## refracts an incoming ray out.
         # We assume the ray is coming in from the right and out of the left.
-        intersection1 = Utils.findIntersection(self.lense1, _ray)[0]
-        normal1 = self.lense1.getNormal(intersection1[0], intersection1[1])
+        intersection1 = Utils.findIntersection(self.lens1, _ray)[0]
+        normal1 = self.lens1.getNormal(intersection1[0], intersection1[1])
         inner_ray = Utils.snells2(_ray, normal1, self.n_air, self.n_glass)
 
         # Now we calculate the ray that comes out of the left side.
-        intersection2 = Utils.findIntersection(self.lense2, inner_ray)[1]
-        normal2 = self.lense2.getNormal(intersection2[0], intersection2[1])
+        intersection2 = Utils.findIntersection(self.lens2, inner_ray)[1]
+        normal2 = self.lens2.getNormal(intersection2[0], intersection2[1])
         exit_ray = Utils.snells2(inner_ray, normal2, self.n_glass, self.n_air)
 
         return exit_ray
 
 
-## The lense, point source object and sensor all in one place.
+## The lens, point source object and sensor all in one place.
 # descriptions of new params.
-# D2: Distance from backside of lense to sensor.
+# D2: Distance from backside of lens to sensor.
 class CameraModel:
     def __init__(self, _R1, _R2, _T, _OD, _D2, _h, _M, _D):
         ## Constructer.
-        # @param _R1 Radius of lense on subject side.
-        # @param _R2 Radius of lense on sensor side.
-        # @param _T  Thickness of lense at center
-        # @param _OD The aperture of the lense.
-        # @param _D2 Distance from back of lense to sensor.
+        # @param _R1 Radius of lens on subject side.
+        # @param _R2 Radius of lens on sensor side.
+        # @param _T  Thickness of lens at center
+        # @param _OD The aperture of the lens.
+        # @param _D2 Distance from back of lens to sensor.
         # @param _h  Height of sensor.
         # @param _M  Number pixels on sensor (M x M)
-        # @param _D  Distance from point source to front of lense.
-        self.lense = Lense(_R1, _R2, _T, _OD)
+        # @param _D  Distance from point source to front of lens.
+        self.lens = Lens(_R1, _R2, _T, _OD)
         self.sensor = Sensor(_h, _M)
         self.T = _T
         self.sensor_pos = -_T / 2 - _D2
         self.source_pos = _T / 2 + _D
-        self.lense_miss = 0
+        self.lens_miss = 0
 
     def sample_ray(self, _theta):
         ## Fire a ray form the point source and record where it hits the
         # sensor.
         # @param _theta The angle at which it is fired.
         ray_in = Ray((self.source_pos, 0), (-1, tan(_theta)))
-        ray_out = self.lense.refract(ray_in)
+        ray_out = self.lens.refract(ray_in)
 
         # check if we hit the sensor.
         sensorYPos = ray_out.getY(self.sensor_pos)
         if abs(sensorYPos) > self.sensor.h:
-            self.lense_miss += 1
-            print("CameraModel::sample_ray() ray missed lense. count")
+            self.lens_miss += 1
+            print("CameraModel::sample_ray() ray missed lens. count")
             return
         else:
             self.sensor.write(sensorYPos)
@@ -152,7 +152,7 @@ class CameraModel:
         # @param _N number of rays.
 
         # calculate max angle by using following hueristic.
-        theta_max = atan(self.lense.OD / (self.source_pos + self.T))
+        theta_max = atan(self.lens.OD / (self.source_pos + self.T))
         thetas = np.linspace(-theta_max, theta_max, num = _N)
 
         for theta in thetas:
